8c28a458416a1ffc4dbe52b28080b967ceb24874
Serhii Humeniuk <gumenyuk.sergey.first@gmail.com>
2023-03-02 08:30:47 +0200

    lucky ticket

--- lucky_ticket.py
+++ lucky_ticket.py
@@ -2,15 +2,15 @@
 
 def lucky_ticket():
     count = 0
-    for number in range(1001, 1011):
+    for number in range(1, 999999):
         lst = [int(x) for x in str(number)]
         while len(lst) < 6:
             lst.append(0)
-
-        print(lst, type(lst), type(lst[3]))
-    return ''
+        if lst[0]+lst[1]+lst[2] == lst[3]+lst[4]+lst[5]:
+            # print(f'{number} is a lucky ticket')
+            count += 1
+    print(f'Number of lucky tickets: {count}')
 
 
 if __name__ == '__main__':
-
     lucky_ticket()
